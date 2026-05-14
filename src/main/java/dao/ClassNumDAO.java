30a5eeb05980c876f7495d140dc8797103f37f22
yooooosay <tdn2532044@stu.o-hara.ac.jp>
2026-05-14 12:10:06 +0900

    a

---
+++ src/main/java/dao/ClassNumDAO.java
@@ -0,0 +1,33 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.ClassNum;
+import bean.School;
+
+public class ClassNumDAO extends DAO {
+	
+	public List<ClassNum> get(School keyword) throws Exception {
+		Connection con = getConnection();
+		List<ClassNum> classlist = new ArrayList<>();
+		
+		PreparedStatement st = con.prepareStatement("select * from class_num where school_cd like ?");
+		st.setString(1,keyword.getCd());
+		ResultSet rs = st.executeQuery();
+		
+		while (rs.next()) {
+			ClassNum classNum = new ClassNum();
+			classNum.setClassNum(rs.getString("Class_Num"));
+			classlist.add(classNum); 
+		}
+		
+		st.close();
+		con.close();
+		
+		return classlist;
+	}
+}
