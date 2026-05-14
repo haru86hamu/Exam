3632063c5167f0604f58928c82c0025908945aad
yooooosay <tdn2532044@stu.o-hara.ac.jp>
2026-05-14 12:18:39 +0900

    あ

--- src/main/java/test/CreateExecuteAction.java
+++ src/main/java/test/CreateExecuteAction.java
@@ -3,10 +3,7 @@ package test;
 import java.util.ArrayList;
 import java.util.List;
 
-import bean.ClassNum;
 import bean.School;
-import bean.Student;
-import bean.Subject;
 import bean.Teacher;
 import bean.Test;
 import dao.TestDAO;
@@ -22,64 +19,27 @@ public class CreateExecuteAction extends Action {
 		HttpSession session = req.getSession();
 		Teacher teacher = (Teacher) session.getAttribute("teacher");
 		School keyword = teacher.getSchool();
+		List<Test> list = new ArrayList<>();
 		TestDAO dao = new TestDAO();
-		String[] no = req.getParameterValues("studentNo");
-		String[] classnum = req.getParameterValues("classNum");
-		String[] point = req.getParameterValues("point");
-		String subject = req.getParameter("subject");
+		Test test = new Test();
+		int entYear = Integer.parseInt(req.getParameter("year"));
+		String no = req.getParameter("no");
+		String name = req.getParameter("name");
+		String classnum = req.getParameter("classnum");
+		int score = Integer.parseInt(req.getParameter("score"));
 		int count = Integer.parseInt(req.getParameter("count"));
-
-		boolean ins = false;		
-			
-		for (int i = 0; i < no.length; i++) {
-			List<Test> list = new ArrayList<>();
-			List<Test> testList = dao.get(no[i], subject, count);
-			Student student = new Student();
-			ClassNum classNum = new ClassNum();
-			Subject sub = new Subject();
-			Test t = new Test();
-
-			student.setNo(no[i]);
-			classNum.setClassNum(classnum[i]);
-			sub.setCd(subject);
-			
-			if (testList.isEmpty()) {
-				t.setStudent(student);
-				t.setSubject(sub);
-				t.setSchool(keyword);
-				t.setNo(count);
-				t.setPoint(Integer.parseInt(point[i]));
-				t.setClassNum(classNum);
-				list.add(t);
-				ins = dao.insert(list);
-				if (ins == false) {
-					req.setAttribute("message", "成績を登録できませんでした。");
-					req.getRequestDispatcher("TestCreate.action").forward(req, resp);
-				}
-			} else {
-				t.setStudent(student);
-				t.setSubject(sub);
-				t.setSchool(keyword);
-				t.setNo(count);
-				t.setPoint(Integer.parseInt(point[i]));
-				t.setClassNum(classNum);
-				list.add(t);
-				ins = dao.update(list);
-				if (ins == false) {
-					req.setAttribute("message", "成績を登録できませんでした。");
-					req.getRequestDispatcher("TestCreate.action").forward(req, resp);
-				}
-			}
-		}
-
+		String subject = req.getParameter("subject");
 		
-		if (ins == true) {
-			req.getRequestDispatcher("testcreate_done.jsp").forward(req, resp);
-		} else {
-			req.setAttribute("message", "成績を登録できませんでした。");
+		
+		/*
+		int line = dao.insert(,keyword);
+		if (line > 0) {
+			req.getRequestDispatcher("").forward(req, resp);
+		}else {
+			String massege = "0∼100の範囲で入力して下さい";
+			req.setAttribute("massege",massege);
 			req.getRequestDispatcher("TestCreate.action").forward(req, resp);
-		}
-
+		}*/
 	}
 
 }
--- src/main/java/test/TestListAction.java
+++ src/main/java/test/TestListAction.java
@@ -5,36 +5,31 @@ import java.util.List;
 import bean.ClassNum;
 import bean.School;
 import bean.Student;
-import bean.Subject;
 import bean.Teacher;
 import dao.ClassNumDAO;
 import dao.StudentDAO;
-import dao.SubjectDAO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
-public class TestCreateAction extends Action {
+public class TestListAction extends Action {
 
 	public void execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
 
 		HttpSession session = req.getSession();
-		Teacher teacher = (Teacher) session.getAttribute("teacher");
-		School keyword = teacher.getSchool();
 		StudentDAO dao = new StudentDAO();
 		ClassNumDAO classdao = new ClassNumDAO();
-		SubjectDAO sub = new SubjectDAO();
+		Teacher teacher = (Teacher) session.getAttribute("teacher");
+		School keyword = teacher.getSchool();
 		String entYear = req.getParameter("year");
 		String classnum = req.getParameter("classnum");
 		String attend = req.getParameter("attend");
 		List<Student> list = dao.search(keyword, entYear, classnum, attend);
 		List<ClassNum> classlist = classdao.get(keyword);
-		List<Subject> subjects = sub.search(keyword);
 		req.setAttribute("classlist",classlist);
 		req.setAttribute("list", list);
-		req.setAttribute("subjects", subjects);
-		req.getRequestDispatcher("testcreate.jsp").forward(req, resp);
+		req.getRequestDispatcher("testlist.jsp").forward(req, resp);
 	}
 
 }
